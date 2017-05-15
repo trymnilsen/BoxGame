750599dab570b53318815b15328c693a8d6b7fe0
Trym Nilsen <trym.nilsen@gmail.com>
2017-05-15 14:54:30 +0200

    Interpolate positions

--- src/ts/client/actor/opponent.ts
+++ src/ts/client/actor/opponent.ts
@@ -1,7 +1,9 @@
 import { BoxCharacter } from './boxCharacter';
 export class Opponent extends BoxCharacter {
+    public static readonly updateRate: number = 200;
     private target: BABYLON.Vector3;
-
+    private lastTarget: BABYLON.Vector3;
+    private totalDeltaTime: number = 0;
     public constructor(mesh: BABYLON.AbstractMesh) {
         super(mesh);
         //Spawn at a random place
@@ -9,14 +11,22 @@ export class Opponent extends BoxCharacter {
         let z = Math.random() * 400 - 200;
         mesh.position = new BABYLON.Vector3(x,20,z);
         this.target = null;
+        this.lastTarget = null;
     }
     public updatePositionAndOrientation(x:number,y:number,z:number,heading:number) {
+        this.lastTarget = this.target;
         let position = new BABYLON.Vector3(x,y,z);
         this.boxMesh.rotation.y = heading;
-         this.boxMesh.position = position; 
         this.target = position;
+        this.totalDeltaTime = 0;
     }
     public update(deltaTime:number) {
+        if(this.lastTarget !== null && this.target !== null) {
+            this.totalDeltaTime += deltaTime * 1000;
+            let t = this.totalDeltaTime / Opponent.updateRate;
+            if(t>1){t = 1;}
+            this.boxMesh.position = BABYLON.Vector3.Lerp(this.lastTarget,this.target,t);
+        }
         // if(this.target!=null){
             
         //     let velocity = this.target.subtract(this.boxMesh.position);
